0e54ad8b4eecaeaed0a1a77631c790d60e37026f
Prakhar Gupta <58157064+guptaprakhariitr@users.noreply.github.com>
2020-06-07 01:03:21 +0530

    Delete urls.py

--- blog/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import api_detail,api_create,api_delete,api_update
-
-urlpatterns = [
-    path('',api_detail,name='api-blog-home'),
-    path('<int:pk>/update',api_update,name='api-blog-update'),
-    path('<int:pk>/delete',api_delete,name='api-blog-delete'),
-    path('create',api_create,name='api-blog-create') ,   
-]
